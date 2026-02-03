343d6d9260e7d4275bd276ada8d06f07b184e685
shristisingh19feb-art <shristisingh19feb@gmail.com>
2026-02-03 10:07:39 +0530

    day 1

--- js day 9/script.js
+++ js day 9/script.js
@@ -1,3 +1,14 @@
-const query = document.querySelector('#content p');
-console.log(query);
-query.style.color = 'blue'; 
+//const query = document.querySelector('#content p');
+//console.log(query);
+//query.style.color = 'blue'; 
+
+
+
+
+//query selectorAll
+const query2 =document.querySelectorAll("p");
+console.log(query2,"query");
+
+for(let i = 0;i<query2.length;i++){
+    query2[i].style.color="green";
+}   
